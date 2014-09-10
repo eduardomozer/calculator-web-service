2c925473fd1f94257509b1e2adad5968f0283fb5
EduardoCesarMozer <eduardocesarmozer@gmail.com>
2014-09-10 11:32:14 -0300

    Serviço web de calculadora.

---
+++ LearnWebService/LearnWebService/Calculadora.asmx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace LearnWebService
+{
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class Calculadora : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public string Hello()
+        {
+            return "Olá mundo WebService!";
+        }
+
+        [WebMethod]
+        public string Chamar()
+        {
+            return "Chamando WebService.";
+        }
+
+        [WebMethod]
+        public string RealizaCalculo(string Operacao, string ValorUm, string ValorDois)
+        {
+            string RetornoCalculo = "";
+
+            switch(Operacao)
+            {
+                case "Soma":
+                    Int32 Soma = Convert.ToInt32(ValorUm) + Convert.ToInt32(ValorDois);
+                    RetornoCalculo = Convert.ToString(Soma);
+                    break;
+
+                case "Subtracao":
+                    Int32 Subtracao = Convert.ToInt32(ValorUm) - Convert.ToInt32(ValorDois);
+                    RetornoCalculo = Convert.ToString(Subtracao);
+                    break;
+
+                case "Multiplicacao":
+                    Int32 Multiplicacao = Convert.ToInt32(ValorUm) * Convert.ToInt32(ValorDois);
+                    RetornoCalculo = Convert.ToString(Multiplicacao);
+                    break;
+
+                case "Divisao":
+                    Int32 Divisao = Convert.ToInt32(ValorUm) / Convert.ToInt32(ValorDois);
+                    RetornoCalculo = Convert.ToString(Divisao);
+                    break;
+            }
+
+            return RetornoCalculo;
+        }
+    }
+}
